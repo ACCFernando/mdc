163c5f98596779fa62af6a465637504584384b81
ACCFernando <fernando.candalaft@gmail.com>
2024-02-16 21:44:38 -0300

    final recup info trab 2

--- recup_info/trabalho 2/inf0611_trabalho2.R
+++ recup_info/trabalho 2/inf0611_trabalho2.R
@@ -38,7 +38,6 @@ plot(load.image(path_imgs[name_imgs[1]]), axes = FALSE, main = name_imgs[1])
 #faz a leitura das imagens com a função do arquvio trabalho2_base.R
 imagens <- read_images(path_plantas)
 
-
 #----------------------------------------------------------------#
 # Obtem classe de cada imagem             
 #----------------------------------------------------------------#
@@ -48,11 +47,11 @@ nome_classes <- get_classes(path_plantas);nome_classes
 # obtem ground_truth para cada classe 
 #----------------------------------------------------------------#
 #classifica com 1 todos os arquivos que são da planta relevante
-ground_truth_biloba <- get_ground_truth(path_plantas,nome_classes,"biloba");ground_truth_biloba
-ground_truth_europaea <- get_ground_truth(path_plantas,nome_classes,"europaea");ground_truth_europaea
-ground_truth_ilex <- get_ground_truth(path_plantas,nome_classes,"ilex");ground_truth_ilex
-ground_truth_monogyna <- get_ground_truth(path_plantas,nome_classes,"monogyna");ground_truth_monogyna
-ground_truth_regia <- get_ground_truth(path_plantas,nome_classes,"regia");ground_truth_regia
+ground_truth_biloba <- get_ground_truth(path_plantas,nome_classes,"biloba")
+ground_truth_europaea <- get_ground_truth(path_plantas,nome_classes,"europaea")
+ground_truth_ilex <- get_ground_truth(path_plantas,nome_classes,"ilex")
+ground_truth_monogyna <- get_ground_truth(path_plantas,nome_classes,"monogyna")
+ground_truth_regia <- get_ground_truth(path_plantas,nome_classes,"regia")
 
 #----------------------------------------------------------------#
 # Questao 1                               
@@ -146,6 +145,8 @@ rownames(features_t) <- names(imagens)
 features_s <- t(sapply(imagens, Momentos))
 rownames(features_s) <- names(imagens)
 
+head(features_c)
+
 #----------------------------------------------------------------#
 # Questao 2                               
 #----------------------------------------------------------------#
@@ -169,32 +170,25 @@ mostrarImagemColorida(consulta_regia)
 #-----------------------------#
 # construindo rankings                          
 # para cada uma das 5 consultas, construa um ranking com base na cor
-hist_cor_biloba <- hist_cor_desc(load.image(consulta_biloba))
-hist_cor_europaea <- hist_cor_desc(load.image(consulta_europaea)); hist_cor_europaea
-hist_cor_ilex <- hist_cor_desc(load.image(consulta_ilex)); hist_cor_ilex
-hist_cor_monogyna <- hist_cor_desc(load.image(consulta_monogyna)); hist_cor_monogyna
-hist_cor_regia <- hist_cor_desc(load.image(consulta_regia)); hist_cor_regia
-#parei aqui
-#aqui esta dando Null, pq??
-ranking_c_biloba <- get_ranking_by_distance(hist_cor_biloba,features_c); ranking_c_biloba
-ranking_c_europaea <- <to-do>
-ranking_c_ilex <- <to-do>
-ranking_c_monogyna <- <to-do>
-ranking_c_regia <- <to-do>
+ranking_c_biloba <- get_ranking_by_distance(features_c,consulta_biloba)
+ranking_c_europaea <- get_ranking_by_distance(features_c,consulta_europaea)
+ranking_c_ilex <- get_ranking_by_distance(features_c,consulta_ilex)
+ranking_c_monogyna <- get_ranking_by_distance(features_c,consulta_monogyna)
+ranking_c_regia <- get_ranking_by_distance(features_c,consulta_regia)
 
 # para cada uma das 5 consultas, construa um ranking com base na textura
-ranking_t_biloba <- <to-do>
-ranking_t_europaea <- <to-do>
-ranking_t_ilex <- <to-do>
-ranking_t_monogyna <- <to-do>
-ranking_t_regia <- <to-do>
+ranking_t_biloba <- get_ranking_by_distance(features_t,consulta_biloba)
+ranking_t_europaea <- get_ranking_by_distance(features_t,consulta_europaea)
+ranking_t_ilex <- get_ranking_by_distance(features_t,consulta_ilex)
+ranking_t_monogyna <- get_ranking_by_distance(features_t,consulta_monogyna)
+ranking_t_regia <- get_ranking_by_distance(features_t,consulta_regia)
   
 # para cada uma das 5 consultas, construa um ranking com base na forma
-ranking_s_biloba <- <to-do>
-ranking_s_europaea <- <to-do>
-ranking_s_ilex <- <to-do>
-ranking_s_monogyna <- <to-do>
-ranking_s_regia <- <to-do>
+ranking_s_biloba <- get_ranking_by_distance(features_s,consulta_biloba)
+ranking_s_europaea <- get_ranking_by_distance(features_s,consulta_europaea)
+ranking_s_ilex <- get_ranking_by_distance(features_s,consulta_ilex)
+ranking_s_monogyna <- get_ranking_by_distance(features_s,consulta_monogyna)
+ranking_s_regia <- get_ranking_by_distance(features_s,consulta_regia)
 
 #-----------------------------#
 # comparando  rankings                              
@@ -204,17 +198,42 @@ ranking_s_regia <- <to-do>
 # top 5, 10, 15 e 20
 
 analyse_rankings <- function(ranking, ground_truth) {
-  <to-do>
+  rankings <- data.frame()  
+  #para cada tamaho de amostra solicitado
+  for(k in c(5,10,15,20)){
+    #usa as funções do arquivo ranking_metrics.R
+    precisao <- precision(ground_truth, ranking, k)
+    revocacao <- recall(ground_truth, ranking, k)
+    taxa_f1 <- f1_score(ground_truth, ranking, k)
+    precisao_media <- ap(ground_truth, ranking, k)
+    #salva cada métrica em uma linha e em seguida empilha em um dataframe
+    linha <- c(k, precisao, revocacao, taxa_f1, precisao_media)
+    rankings <- rbind(rankings, linha)
+  }
+  names(rankings) <- c("amostra", "precisao", "revocacao", "taxa_f1", "precisao_media")
+  print(rankings)
 }
 
 # analisando rankings gerados com caracteristicas de cor
-analyse_rankings(<to-do>)
+analyse_rankings(ranking_c_biloba, ground_truth_biloba)
+analyse_rankings(ranking_c_europaea, ground_truth_europaea)
+analyse_rankings(ranking_c_ilex, ground_truth_ilex)
+analyse_rankings(ranking_c_monogyna, ground_truth_monogyna)
+analyse_rankings(ranking_c_regia, ground_truth_regia)
 
 # analisando rankings gerados com caracteristicas de textura
-analyse_rankings(<to-do>)
+analyse_rankings(ranking_t_biloba, ground_truth_biloba)
+analyse_rankings(ranking_t_europaea, ground_truth_europaea)
+analyse_rankings(ranking_t_ilex, ground_truth_ilex)
+analyse_rankings(ranking_t_monogyna, ground_truth_monogyna)
+analyse_rankings(ranking_t_regia, ground_truth_regia)
 
 # analisando rankings gerados com caracteristicas de forma
-analyse_rankings(<to-do>)
+analyse_rankings(ranking_s_biloba, ground_truth_biloba)
+analyse_rankings(ranking_s_europaea, ground_truth_europaea)
+analyse_rankings(ranking_s_ilex, ground_truth_ilex)
+analyse_rankings(ranking_s_monogyna, ground_truth_monogyna)
+analyse_rankings(ranking_s_regia, ground_truth_regia)
 
 
 #----------------------------------------------------------------#
@@ -226,22 +245,22 @@ analyse_rankings(<to-do>)
 # aponte pontos fortes e fracos dos descritores usados que podem
 # justificar esse comportamento.
 #                                         
-#                                         
-#                                         
-#                                         
-#                                         
-#                                         
+# Escolhendo a amostra de Europaea, observa-se que essa consulta teve o melhor descritor                                        
+# sendo o descritor de forma, apresentando f1 alto, e rapido aumento na revocacao. Isso pode                                         
+# ser justificado pela uniformidade na forma da planta, com poucas nuances nas imagens, variando mais em cor do que em forma.
+# Os pontos fracos do descritor de forma é que plantas de formas similares seriam classificadas no topo do ranking, mesmo sendo de cores
+# e texturas muito diferentes
+#
 # (b) Considerando as 5 consultas definidas, calcule a média das
 # precisões médias em top 10. Avaliando essa medida, qual descritor
 # obteve melhores rankings? 
-#                                         
-#                                         
-#                                         
-#                                         
-#                                         
-#                                         
-#                                         
-#                                         
+#
+# Ao fazer as médias das precisões médias obtém-se:
+mean(analyse_rankings(ranking_c_europaea, ground_truth_europaea)$precisao_media) #0.7
+mean(analyse_rankings(ranking_t_europaea, ground_truth_europaea)$precisao_media) #0.9
+mean(analyse_rankings(ranking_s_europaea, ground_truth_europaea)$precisao_media) #1.0
+
+#O descritor que obteve os melhores rankings foi o de forma
 #                                         
 #----------------------------------------------------------------#
 
@@ -252,33 +271,39 @@ analyse_rankings(<to-do>)
 
 ## obter vetores finais de caracteristicas pela concatenação de 
 # cada tipo de caracteristica (cor, textura e forma):
-features_concat = <to-do>
-  
+#empilha coluna a coluna
+features_concat <- cbind(features_c,features_t,features_s)
+
 # gerar novos rankings
-ranking_concat_biloba <- <to-do>
-ranking_concat_europaea <- <to-do>
-ranking_concat_ilex <- <to-do>
-ranking_concat_monogyna <- <to-do>
-ranking_concat_regia <- <to-do>
+ranking_concat_biloba <- get_ranking_by_distance(features_concat,consulta_biloba)
+ranking_concat_europaea <- get_ranking_by_distance(features_concat,consulta_europaea)
+ranking_concat_ilex <-  get_ranking_by_distance(features_concat,consulta_ilex)
+ranking_concat_monogyna <-  get_ranking_by_distance(features_concat,consulta_monogyna)
+ranking_concat_regia <-  get_ranking_by_distance(features_concat,consulta_regia)
+
 
 # analisando rankings gerados com caracteristicas concatenadas
-analyse_rankings(<to-do>)
-
+analyse_rankings(ranking_concat_biloba, ground_truth_biloba)
+analyse_rankings(ranking_concat_europaea, ground_truth_europaea)
+analyse_rankings(ranking_concat_ilex, ground_truth_ilex)
+analyse_rankings(ranking_concat_monogyna, ground_truth_monogyna)
+analyse_rankings(ranking_concat_regia, ground_truth_regia)
 #----------------------------------------------------------------#
 # Questao 3 - RESPONDA:  
 # (a) Qual o impacto dessas alterações nas medidas de avaliação
 # calculadas?
-# 
+# As métricas apresentaram queda em seus valores.
 # 
 # (b) Os descritores combinados apresentaram melhores resultados?
 # Justifique sua resposta.
-# 
-# 
-# 
+# Os descritores combinados não apresentam melhores resultados do que sua avaliação individual,
+# todas as métricas apresentaram quedas para todas as amostras de plantas. Isso pode ocorrer 
+# devido ao aumento da complexidade do vetor, juntando várias features ao mesmo tempo.
 # 
 # (c) Você acredita que algum dos descritores apresentou maior
 # influência na combinação? Justifique sua resposta.
 # 
-# 
-# 
+# O descritor de textura, que possuia métricas mais baixas na avaliação comparado aos outros descritores
+# Isso pode ocorrer porque não há considerável diferença de textura entre as plantas relativamente à sua aspereza.
+# Portanto textura pode ter sido o descritor que abaixou as métricas ao combinar com os outros.
 #----------------------------------------------------------------#
